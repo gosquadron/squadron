a6ec134aee447960118a351c6c412b2d20adedac
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-11-02 14:46:23 -0700

    main apply now does state additions

--- squadron/main.py
+++ squadron/main.py
@@ -4,6 +4,7 @@ import jsonschema
 from jsonschema import Draft4Validator, validators
 import tempfile
 from template import DirectoryRender
+from state import StateHandler
 
 # This will be easy to memoize if need be
 def get_service_json(squadron_dir, service_name, service_ver, filename):
@@ -12,11 +13,22 @@ def get_service_json(squadron_dir, service_name, service_ver, filename):
         return json.loads(jsonfile.read())
 
 def apply(squadron_dir, node_info, dry_run=False):
+    """
+    This method takes input from the given squadron_dir and configures
+    a temporary directory according to that information
+
+    Keyword arguments:
+        squadron_dir -- configuration directory for input
+        node_info -- dictionary of this node's information
+        dry_run -- whether or not to actually create the temp directory
+            or change any system-wide configuration via state.json
+    """
     conf_dir = os.path.join(squadron_dir, 'config', node_info['env'])
 
     result = {}
+    library_dir = os.path.join(squadron_dir, 'library')
+    state = StateHandler(library_dir)
     for service in node_info['services']:
-        cfg = {}
         with open(os.path.join(conf_dir, service + '.json'), 'r') as cfile:
             metadata = json.loads(cfile.read())
             version = metadata['version']
@@ -41,6 +53,11 @@ def apply(squadron_dir, node_info, dry_run=False):
             render.render(tmpdir, cfg)
 
             result[service] = tmpdir
+
+        statejson = get_service_json(squadron_dir, service, version, 'state')
+        for library, items in statejson.items():
+            state.apply(library, items, dry_run)
+
 
     return result
 
--- squadron/tests/test_apply.py
+++ squadron/tests/test_apply.py
@@ -2,6 +2,11 @@ from .. import main
 import pytest
 import jsonschema
 from helper import are_dir_trees_equal
+import os
+
+def checkfile(filename, compare):
+    with open(filename) as ofile:
+        assert compare == ofile.read()
 
 def test_basic():
     node = {'services' : ['api'], 'env' : 'dev'}
@@ -9,6 +14,11 @@ def test_basic():
 
     assert len(results) == 1
     assert are_dir_trees_equal(results['api'], 'applytests/applytest1result')
+
+    checkfile('/tmp/test1.out', '55')
+    checkfile('/tmp/test2.out', '0')
+    os.remove('/tmp/test1.out')
+    os.remove('/tmp/test2.out')
 
 def test_schema_validation_error():
     node = {'services' : ['api'], 'env' : 'dev'}
