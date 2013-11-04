76337446a64c312dbd04994a405391ce9eda88bc
Gus <gus@guscatalano.com>
2013-11-04 02:31:39 +0000

    initialize from git repro or skeleton

--- squadron/initialize.py
+++ squadron/initialize.py
@@ -2,13 +2,37 @@ import os
 import json
 import jsonschema
 import template
+import subprocess
+from git import *
 
-def init(squadron_dir):
-    os.makedirs(os.path.join(squadron_dir, 'libraries'))
-    os.makedirs(os.path.join(squadron_dir, 'config'))
-    os.makedirs(os.path.join(squadron_dir, 'services'))
-    os.makedirs(os.path.join(squadron_dir, 'nodes'))
-    os.makedirs(os.path.join(squadron_dir, 'inputchecks'))
+def init(squadron_dir, skeleton, gitrepro):
+    if(os.path.exists(squadron_dir)):
+        print "Directory already exists, please provide a new directory"
+        exit(1)
+        #TODO: Consider adding a FORCE flag if it matters
+
+    os.makedirs(squadron_dir)
+    ret = True
+    if skeleton == True:
+        os.makedirs(os.path.join(squadron_dir, 'libraries'))
+        os.makedirs(os.path.join(squadron_dir, 'config'))
+        os.makedirs(os.path.join(squadron_dir, 'services'))
+        os.makedirs(os.path.join(squadron_dir, 'nodes'))
+        os.makedirs(os.path.join(squadron_dir, 'inputchecks'))
+        repo = True
+    elif gitrepro == None:
+        print "Using community repro..."
+        repo = Repo.clone_from("git@github.com:guscatalano/squadron-init.git", squadron_dir)
+    else:
+        print "Using custom repro"
+        repo = Repo.clone_from(gitrepro, squadron_dir)
+
+    ret = repo != None
+    if ret:
+        print "Squadron has been initialized"   
+    else:
+        print "Squadron init seems to have failed"
+
 
 def initService(squadron_dir, service_name):
     os.makedirs(os.path.join(squadron_dir, service_name, 'root'))
