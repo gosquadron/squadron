f4108f90d99b2ffa8313974b5c3e695adcf7630a
Gus <gus@guscatalano.com>
2013-11-09 04:03:04 +0000

    daemon registers with directory

--- squadron/daemon.py
+++ squadron/daemon.py
@@ -1,2 +1,15 @@
-def run(home_dir, hub):
-    print home_dir
+import urllib2
+import socket
+
+def run(home_dir, hub="http://provehito.com:8081"):
+
+    if(hub == None or hub.find('http://') == -1):
+        hub = "http://provehito.com:8081"
+    url = (hub + "/register/" + str(socket.gethostname()))
+    print url
+    response = urllib2.urlopen(url).read()
+    if(response == 'ok'):
+        print "registered :)"
+    else:
+        print "could not register? :("
+    
