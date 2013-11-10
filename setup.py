e3aee416fe71ebd7a0db0575423c1ee98b7eede0
Gus <gus@guscatalano.com>
2013-11-10 05:14:30 +0000

    install community repo to site packages

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-
 setup(
     name='squadron',
     version='0.0.1',
@@ -9,3 +8,16 @@ setup(
     install_requires=['jsonschema','gitpython','quik']
 )
 
+#hopefully its been installed now
+from git import *
+import os
+from distutils.sysconfig import get_python_lib
+root_dir = os.path.join(get_python_lib(), "squadron")
+if not (os.path.exists(root_dir)):
+    print "Squadron did not install in site-packages :("
+    exit(1)
+
+community_dir = os.path.join(root_dir, "community")
+if not (os.path.exists(community_dir)):
+    #install only if it already doesn't exist (from previous install)
+    Repo.clone_from("https://github.com/guscatalano/squadron-init.git", community_dir)
