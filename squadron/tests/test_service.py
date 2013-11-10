e1f04b9521c348a3e114127fc23ea59060bbc3c0
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-11-10 15:12:22 -0800

    Test for get_reactions

--- squadron/tests/test_service.py
+++ squadron/tests/test_service.py
@@ -22,5 +22,20 @@ def test_get_service_actions():
     assert 'service1.start' in actions['service1.restart']['not_after']
 
 def test_get_reactions():
-    actions = get_reactions('service_tests', 'service1', '1.0.1')
-    print "actions {}".format(actions)
+    reactions = get_reactions('service_tests', 'service1', '1.0.1')
+
+    assert len(reactions) == 3
+
+    start = reactions[0]
+    assert 'service1.start' in start['execute']
+    assert 'apache2.restart' in start['execute']
+    assert len(start['when']['command']) > 0
+    assert start['when']['exitcode'] == 1
+
+    reload = reactions[1]
+    assert 'service1.reload' in reload['execute']
+    assert len(reload['when']['files']) > 0
+
+    restart = reactions[2]
+    assert 'service1.restart' in restart['execute']
+    assert len(restart['when']['files']) > 0
