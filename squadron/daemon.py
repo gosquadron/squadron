149a4de48d45c466f6b9513b8d59a3229cf8ba27
Gus <gus@guscatalano.com>
2013-11-12 02:03:35 +0000

    pass parameters to daemon correctly

--- squadron/daemon.py
+++ squadron/daemon.py
@@ -1,5 +1,9 @@
 import urllib2
 import socket
+from git import *
+import os
+from squadron import main
+import time
 
 def run(home_dir, gitrepro, hub="http://provehito.com:8081"):
 
@@ -7,21 +11,36 @@ def run(home_dir, gitrepro, hub="http://provehito.com:8081"):
         print "please pass a git repro to poll"
         exit(1)
 
-    repro = Repo.clone_from(gitrepro, home_dir)
+    if not(os.path.exists(os.path.join(home_dir, ".git"))):
+        repro = Repo.clone_from(gitrepro, home_dir)
+    else:
+        repro = Repo(home_dir)
     
 
     if(hub == None or hub.find('http://') == -1):
         hub = "http://provehito.com:8081"
     url = (hub + "/register/" + str(socket.gethostname()))
-    print url
-    response = urllib2.urlopen(url).read()
+    response = ':('
+    try: 
+        response = urllib2.urlopen(url).read()
+    except:
+        pass
     if(response == 'ok'):
         print "registered :)"
     else:
         print "could not register? :("
-    
+
+    lastCommit = ""
     while(True):
         
-        git.pull(refspec="refs/heads/master:refs/remotes/origin/master")
-        print git.refs.master
-
+        repro.remote(name='origin').pull(refspec="refs/heads/master:refs/remotes/origin/master")
+        currentCommit = repro.iter_commits().next()
+        if(currentCommit != lastCommit):
+            lastCommit = currentCommit
+            print "updating node"
+            try:
+                main.apply(home_dir, socket.getfqdn())
+            except:
+                pass
+        #Do heartbeat
+        time.sleep(30)
