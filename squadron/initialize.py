e2a1c46b60ced4210bb5a3f643d5d352c94c6e33
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-11-13 08:48:13 -0800

    Only initialize repo if we're not cloning

--- squadron/initialize.py
+++ squadron/initialize.py
@@ -13,32 +13,32 @@ def init(squadron_dir, skeleton, gitrepo, force=False, example=False):
         return False
 
     makedirsp(squadron_dir)
-    ret = True
-    if skeleton == True and gitrepo != None:
+    if skeleton is True and gitrepo is not None:
         print "Can't do skeleton and gitrepo at the same time"
         return False
 
-    if skeleton == False and gitrepo == None:
+    if skeleton is False and gitrepo is None:
         skeleton = True #Probably a silly mistake
 
-    if skeleton == True or gitrepo == None:
+    if skeleton is True or gitrepo is None:
         print "Creating skeleton..."
         makedirsp(os.path.join(squadron_dir, 'libraries'))
         makedirsp(os.path.join(squadron_dir, 'config'))
         makedirsp(os.path.join(squadron_dir, 'services'))
         makedirsp(os.path.join(squadron_dir, 'nodes'))
 
-    if gitrepo != None:
+    if gitrepo is not None:
         repo = Repo.clone_from(gitrepo, squadron_dir)
+    else:
+        repo = Repo.init(squadron_dir) # initialize repo
 
     print "Squadron has been initialized"
-    if example != True:
+    if example is False:
         print "If this is your first time with Squadron, you should init a service by passing --service [name] to the previous command"
     else:
         init_service(squadron_dir, 'example')
         print "We have init an example service for you, please check out services/example"
 
-    repo = Repo.init(squadron_dir) # initialize repo
     return True
 
 
