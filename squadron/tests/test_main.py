84919c710489d4df241043e5cf95b6274dd469bb
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-12-05 08:14:09 -0800

    Fix main tests, directory wasn't being created

--- squadron/tests/test_main.py
+++ squadron/tests/test_main.py
@@ -3,12 +3,14 @@ import os
 import json
 from ..fileio.dirio import makedirsp
 from helper import are_dir_trees_equal
+import shutil
 
 def test_main_basic(tmpdir):
     tmpdir = str(tmpdir)
 
     squadron_state_dir = os.path.join(tmpdir, 'state')
     makedirsp(squadron_state_dir)
+    makedirsp('/tmp/applytest1/')
 
     squadron_dir = 'main_tests/main1'
 
@@ -35,6 +37,7 @@ def test_main_basic(tmpdir):
 
     assert old_dir != new_dir
     assert are_dir_trees_equal('main_tests/main1result', info['dir']) == True
+    shutil.rmtree('/tmp/applytest1/')
 
 def test_main_git(tmpdir):
     tmpdir = str(tmpdir)
@@ -52,3 +55,4 @@ def test_main_git(tmpdir):
     assert 'dir' in info
     assert os.path.isdir(info['dir']) == True
     assert are_dir_trees_equal('main_tests/main2result', info['dir']) == True
+    shutil.rmtree('/tmp/main2test/')
