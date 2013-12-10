92bead24a886922e79af80cb32eb2641748fa02f
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-12-09 22:03:27 -0800

    Ditch this for now, doesn't work

--- setup.py
+++ setup.py
@@ -8,17 +8,3 @@ setup(
     data_files=[('/etc/squadron',['files/config'])],
     install_requires=['jsonschema','gitpython','quik','requests']
 )
-
-#hopefully its been installed now
-from git import *
-import os
-from distutils.sysconfig import get_python_lib
-root_dir = os.path.join(get_python_lib(), "squadron")
-if not (os.path.exists(root_dir)):
-    print "Squadron did not install in site-packages :("
-    exit(1)
-
-community_dir = os.path.join(root_dir, "community")
-if not (os.path.exists(community_dir)):
-    #install only if it already doesn't exist (from previous install)
-    Repo.clone_from("https://github.com/guscatalano/squadron-init.git", community_dir)
