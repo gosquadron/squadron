896f7b82eb1c84538a94e65d8ff55282e36c6818
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-12-19 08:55:38 -0800

    Fix broken tests because of virtualenv handler

--- squadron/exthandlers/__init__.py
+++ squadron/exthandlers/__init__.py
@@ -2,11 +2,12 @@ from .dir import ext_dir
 from .makegit import ext_git
 from .download import ext_download
 from .template import ext_template
+from .virtualenv import ext_virtualenv
 
 extension_handles = {
     'dir':ext_dir,
     'git':ext_git,
     'download':ext_download,
-    'virtualenv',ext_virtualenv,
+    'virtualenv':ext_virtualenv,
     'tpl':ext_template
 }
