d20fd628fbe763e2afc8d79268e229b69c26a44e
Sean Cassidy <sean.a.cassidy@gmail.com>
2014-02-01 14:48:09 -0800

    Clean up virtualenv handler

--- squadron/exthandlers/tests/test_virtualenv.py
+++ squadron/exthandlers/tests/test_virtualenv.py
@@ -14,9 +14,21 @@ def integration(tmpdir):
 
     assert os.path.exists(finalfile)
     assert os.path.exists(os.path.join(finalfile, 'bin', 'activate'))
-    assert os.path.exists(os.path.join(finalfile, 'bin', 'activate'))
 
     lib_dir = os.path.join(finalfile, 'lib', 'python2.7',
                 'site-packages')
     assert os.path.exists(os.path.join(lib_dir, 'bottle.py'))
     assert os.path.exists(os.path.join(lib_dir, 'pytest.py'))
+
+def test_basic(tmpdir):
+    tmpdir = str(tmpdir)
+
+    abs_source = os.path.join(tmpdir, 'requirements.txt')
+    with open(abs_source, 'w') as vfile:
+        pass
+
+    dest = os.path.join(tmpdir, 'env~virtualenv')
+    finalfile = ext_virtualenv(abs_source, dest)
+
+    assert os.path.exists(finalfile)
+    assert os.path.exists(os.path.join(finalfile, 'bin', 'activate'))
--- squadron/exthandlers/virtualenv.py
+++ squadron/exthandlers/virtualenv.py
@@ -5,44 +5,26 @@ from extutils import get_filename
 
 def ext_virtualenv(abs_source, dest, **kwargs):
     requirements=[]
-    with open(abs_source) as vfile:
-        for line in vfile.readlines():
-            if line.startswith('#'):
-                # Metadata or comment
-                1
-            else:
-                requirements.append(line.strip())
 
     # Create virtualenv
     virtual_env = get_filename(dest)
     subprocess.check_call(['virtualenv', virtual_env])
 
-    if requirements:
-        # Write out requirements file
-        fd, req_file = tempfile.mkstemp('.sq','requirements')
+    # Set up environment
+    pip_env = os.environ.copy()
+    pip_env['VIRTUAL_ENV'] = virtual_env
+    pip_env['PATH'] = '{}/bin:{}'.format(virtual_env, pip_env['PATH'])
 
-        with os.fdopen(fd, 'w') as f:
-            for line in requirements:
-                f.write(line + '\n')
+    if 'PYTHONHOME' in pip_env:
+        del pip_env['PYTHONHOME']
 
-        try:
-            # Set up environment
-            pip_env = os.environ.copy()
-            pip_env['VIRTUAL_ENV'] = virtual_env
-            pip_env['PATH'] = '{}/bin:{}'.format(virtual_env, pip_env['PATH'])
+    # Install requirements
+    pip_bin = os.path.join(virtual_env, 'bin', 'pip')
+    pip = subprocess.Popen([pip_bin, 'install', '-r', abs_source],
+            env=pip_env)
+    ret_code = pip.wait()
 
-            if 'PYTHONHOME' in pip_env:
-                del pip_env['PYTHONHOME']
-
-            # Install requirements
-            pip_bin = os.path.join(virtual_env, 'bin', 'pip')
-            pip = subprocess.Popen([pip_bin, 'install', '-r', req_file],
-                    env=pip_env)
-            ret_code = pip.wait()
-
-            if ret_code != 0:
-                raise subprocess.CalledProcessError(ret_code, pip_bin)
-        finally:
-            os.remove(req_file)
+    if ret_code != 0:
+        raise subprocess.CalledProcessError(ret_code, pip_bin)
 
     return virtual_env
