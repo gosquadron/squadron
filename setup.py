48541db119c869eafbf96b16e9c00041b46f78c6
Sean Cassidy <sean.a.cassidy@gmail.com>
2014-02-12 09:05:26 -0800

    0.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class PyTest(TestCommand):
 from setuptools import setup, find_packages
 setup(
     name='squadron',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     license='MIT',
     scripts=['scripts/squadron'],
