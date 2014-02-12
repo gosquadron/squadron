2e18e3cd1a510dc9af3a4debc244006997baa1c9
Sean Cassidy <sean.a.cassidy@gmail.com>
2014-02-12 09:20:26 -0800

    Add PyPI description and such

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools.command.test import test as TestCommand
 import sys
+import os
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -12,12 +13,30 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 from setuptools import setup, find_packages
 setup(
     name='squadron',
     version='0.0.2',
-    packages=find_packages(),
+    author='Squadron',
+    author_email='info@gosquadron.com',
+    description='Easy-to-use configuration and release management tool',
+    long_description=read('README.md'),
     license='MIT',
+    url='http://www.gosquadron.com',
+    keywords='configuration management release deployment tool',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages(),
     scripts=['scripts/squadron'],
     tests_require=[
         'pytest>=2.5.1',
