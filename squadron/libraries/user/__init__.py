1a24f103a4b881173c1be14aa43ce478aa1c7d72
Sean Cassidy <sean.a.cassidy@gmail.com>
2014-02-12 08:58:16 -0800

    Revert "fix one of user lib tests to work in linux"
    This reverts commit 05fb2671219aff92ec3983f75983f146e79c47b3.
    
    This doesn't work on OS X.

--- squadron/libraries/user/__init__.py
+++ squadron/libraries/user/__init__.py
@@ -4,13 +4,6 @@ from string import find
 from squadron.initialize import default_schema
 import pwd
 import grp
-import platform
-
-FAKE_RESULT = False
-
-def set_test_hook(ret):
-    global FAKE_RESULT
-    FAKE_RESULT = ret
 
 def schema():
     """
@@ -85,90 +78,40 @@ def verify(inputhashes):
 
     return failed
 
-
-def apply_linux(inputhashes, log):
-    failed = []
-    for user in inputhashes:
-        args = ['useradd']
-        if 'system' in user:
-            raise EnvironmentError("System not supported in useradd under linux")
-        if 'uid' in user:
-            args.append('-u')
-            args.append(user['uid'])
-        if 'gid' in user:
-            args.append('-g')
-            args.append(user['gid'])
-        if 'shell' in user:
-            args.append('-s')
-            args.append(user['shell'])
-        if 'realname' in user:
-            args.append('--comment')
-            args.append(user['realname'])
-
-        if 'homedir' in user:
-            args.append('-d')
-            args.append(user['homedir'])
-        
-        args.append(user['username'])
-
-        #Test hook
-        print FAKE_RESULT
-        global FAKE_RESULT
-        if FAKE_RESULT != False:
-            return FAKE_RESULT
-        result = subprocess.call(args)
-        if result != 0:
-            failed.append(user)
-    return failed
-
 def apply(inputhashes, log):
     failed = []
     for user in inputhashes:
         username = user['username']
         try:
             result = pwd.getpwnam(username)
-            #Can't modify users that already exist
+            # Can't modify users that already exist...yet
             failed.append(user)
-            return failed
         except KeyError:
-            #Run platform specific useradd
-            if(platform.system() == 'Darwin'):
-                return apply_osx(inputhashes, log)
-            else:
-                return apply_linux(inputhashes, log)
+            # this is the normal path
+            args = ['useradd']
+            if 'system' in user:
+                if bool(user['system']):
+                    args.append('--system')
+            if 'uid' in user:
+                args.append('--uid')
+                args.append(user['uid'])
+            if 'gid' in user:
+                args.append('--gid')
+                args.append(user['gid'])
+            if 'shell' in user:
+                args.append('--shell')
+                args.append(user['shell'])
+            if 'realname' in user:
+                args.append('--comment')
+                args.append(user['realname'])
+            if 'homedir' in user:
+                args.append('--home')
+                args.append(user['homedir'])
 
+            result = subprocess.call(args)
 
-def apply_osx(inputhashes, log):
-    failed = []
-    for user in inputhashes:
-        # this is the normal path
-        args = ['useradd']
-        if 'system' in user:
-            if bool(user['system']):
-                args.append('--system')
-        if 'uid' in user:
-            args.append('--uid')
-            args.append(user['uid'])
-        if 'gid' in user:
-            args.append('--gid')
-            args.append(user['gid'])
-        if 'shell' in user:
-            args.append('--shell')
-            args.append(user['shell'])
-        if 'realname' in user:
-            args.append('--comment')
-            args.append(user['realname'])
-        if 'homedir' in user:
-            args.append('--home')
-            args.append(user['homedir'])
-
-        #Verify this works in osx
-        args.append(user['username'])
-
-        result = subprocess.call(args)
-
-        if result != 0:
-            failed.append(user)
+            if result != 0:
+                failed.append(user)
 
     return failed
 
--- squadron/libraries/user/test_user.py
+++ squadron/libraries/user/test_user.py
@@ -1,6 +1,6 @@
+from . import schema, verify, apply
 import os, platform
 import pytest
-from . import schema, verify, apply, set_test_hook
 
 root = {
     'username':'root',
@@ -12,17 +12,14 @@ non_existant_user = {
     'username':'nonexistantuser333'
 }
 
-#Fix verify to work in linux before this
 @pytest.mark.skipif(platform.system() != 'Darwin', reason="requires osx")
 def test_verify():
     result = verify([root, non_existant_user])
     assert len(result) == 1
     assert result[0] == non_existant_user
 
+@pytest.mark.skipif(platform.system() != 'Darwin', reason="requires osx")
 def test_apply():
-    #If not root
-    if(os.geteuid() != 0):
-        set_test_hook([]) 
     result = verify([root, non_existant_user])
     apply_result = apply(result, "")
     assert len(apply_result) == 0
