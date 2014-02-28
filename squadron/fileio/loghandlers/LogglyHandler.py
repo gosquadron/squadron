c08ad556ede7a5d1ac21e6c73c67c447acb93033
redprophet <gus@guscatalano.com>
2014-02-27 23:30:54 -0800

    fixes to LogglyHandler to make it work :)

--- squadron/fileio/loghandlers/LogglyHandler.py
+++ squadron/fileio/loghandlers/LogglyHandler.py
@@ -4,30 +4,31 @@ import socket
 from logging import Handler
 
 class LogglyHandler(Handler):
-        """
-        Handler that logs to loggly.
-        """
+    """
+    Handler that logs to loggly.
+    """
+    base_url = None
+    def __init__(self, base_url=None):
+        if(base_url is None):
+            self.base_url = 'https://logs-01.loggly.com/inputs/b121e4df-f910-4d6a-b6c1-b19ca2776233/tag/python/'
+        else:
+            self.base_url = base_url
+        self.localip = socket.gethostbyname(socket.gethostname())
+        self.publicip = urllib2.urlopen('http://ip.42.pl/raw').read()
 
-        def __init__(self, base_url=None):
-            if(base_url is None):
-                base_url = 'https://logs-01.loggly.com/inputs/b121e4df-f910-4d6a-b6c1-b19ca2776233/tag/python/'
-            self.localip = socket.gethostbyname(socket.gethostname())
-            self.publicip = urllib2.urlopen('http://ip.42.pl/raw').read()
-
-        on_same_line = False
-        def emit(self, record):
-            try:
-                msg = self.format(record)
-                log_data = "PLAINTEXT=" + urllib2.quote(simplejson.dumps(
-                    {
-                        'msg':msg,
-                        'localip':self.localip,
-                        'publicip':self.publicip,
-                        'tenant':'TODO :)'
-                    }
-                ))
-                urllib2.urlopen(base_url, log_data)
-            except(KeyboardInterrupt, SystemExit):
-                raise
-            except:
-                self.handleError(record)
+    def emit(self, record):
+        try:
+            msg = record.getMessage()
+            log_data = "PLAINTEXT=" + urllib2.quote(simplejson.dumps(
+                {
+                    'msg':msg,
+                    'localip':self.localip,
+                    'publicip':self.publicip,
+                    'tenant':'TODO :)'
+                }
+            ))
+            urllib2.urlopen(self.base_url, log_data)
+        except(KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
